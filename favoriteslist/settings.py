47b871656ed0a906ce2d6b1083fe1ee25baa8f99
Katie Bridson <kbridson@memphis.edu>
2025-04-14 13:58:21 -0500

    Added css and images (static assets)

--- favoriteslist/settings.py
+++ favoriteslist/settings.py
@@ -124,7 +124,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
